52264d6ad3450031abe705492607af7d358b18c2
Jianshu-Hu <jianshuhu98@gmail.com>
2024-03-04 16:39:51 +0800

    implement sac in drqv2

--- drqv2-main/run_experiments/run_cluster_quadruped_run_drq_4.sh
+++ drqv2-main/run_experiments/run_cluster_quadruped_run_drq_4.sh
@@ -4,8 +4,8 @@ cd /bd_targaryen/users/jhu/temporal-invariance/drqv2-main/
 source /bd_targaryen/users/jhu/anaconda3/bin/activate
 conda activate drqv2
 
-tag=drqv2_repeat_2_simhash_count
+tag=drqv2_sac
 seed=4
 
 echo "start running $tag with seed $seed"
-python train.py task=quadruped_run repeat_type=1 update_every_steps=2 nstep=3 action_repeat=2 experiment=$tag seed=$seed num_train_frames=1000000
+python train.py task=quadruped_run alg=drqv2_sac.DrQV2SACAgent repeat_type=0 update_every_steps=2 nstep=3 action_repeat=2 experiment=$tag seed=$seed num_train_frames=1000000
